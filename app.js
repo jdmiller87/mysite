ec47b44ebf21ae37ba12bb4434be4eba10d0f6ce
Maria Chapman <thunderstarry@gmail.com>
2015-04-01 00:43:04 +0100

    Cleaned up node modules and added static serving sample

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-/*
- * Module dependencies
- */
- var express = require('express') ,
-   stylus = require('stylus'),
-   nib = require('nib');
+//Module dependencies
+var express = require('express') ,
+  stylus = require('stylus'),
+  nib = require('nib')
+  jadeStatic = require('connect-jade-static');
   
 var app = express();
 function compile(str, path) {
@@ -15,16 +14,30 @@ function compile(str, path) {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
+
+// Transform CSS
 app.use(stylus.middleware({
     src: __dirname + '/public',
     compile: compile
   }
 ));
+
+// Serve static content
 app.use(express.static(__dirname + '/public'));
 
+// Serve static jadefiles
+app.use(jadeStatic({
+    baseDir: __dirname + '/views',
+    baseUrl: '/',
+    maxAge: 86400,
+    jade: { pretty: true }
+  }
+));
+
+// Redirect users who hit the root to /home
 app.get('/', function (req, res) {
   res.render('home',
-  { title : 'Home' }
+    { title : 'Home' }
   )
 });
 
